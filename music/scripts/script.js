c790d85dcad7f60ba9f4f1de3c55b29e4c8ae9a0
Fvpm <fvpolicastro@gmail.com>
2021-07-17 21:03:10 +0000

    After C10

---
+++ music/scripts/script.js
@@ -0,0 +1,14 @@
+/*  
+Author: Felice Policastro 
+File Name: script.js
+Date: 07/17/2021
+*/
+//Hamburger menu function
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+        } else {
+            menu.style.display = "block";
+        }
+}
